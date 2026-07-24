abc1e202e8b6fce44636756136d0afaf894ada54
nerangerr <rozacat777@gmail.com>
2026-07-24 15:36:14 +0500

    Исправлен порт для Render (10000)

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ def submit():
     return jsonify({"status": "ok", "message": "Данные получены!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
